0e2e5051876e25e421b7a3b2431c63a1bca20bb9
Carlitos <116768102+carlitoswolf@users.noreply.github.com>
2023-10-13 21:27:40 -0600

    Initial Commit
    First Commit in the repository

---
+++ PM023P_Exercises_01/PM023P_Exercises_01/Views/PageListOfPeople.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM023P_Exercises_01.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageListOfPeople : ContentPage
+    {
+        public PageListOfPeople()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.PagePersonas());
+        }
+
+        private void list_SelectionChanged(object sender, SelectedItemChangedEventArgs e)
+        {
+            
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            list.ItemsSource = await App.Instancia.GetListPople();
+        }
+
+    }
+}
