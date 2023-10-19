5c4214ce07396e54c51dbb9bf07939a821fbd222
Carlitos <116768102+carlitoswolf@users.noreply.github.com>
2023-10-18 21:57:58 -0600

    Commit 18/10/2023

--- PM023P_Exercises_01/Views/PageMap.xaml.cs
+++ PM023P_Exercises_01/Views/PageMap.xaml.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 
 namespace PM023P_Exercises_01.Views
 {
@@ -16,5 +19,35 @@ namespace PM023P_Exercises_01.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+			var conexion = Connectivity.NetworkAccess;
+			var locl = CrossGeolocator.Current;
+
+            if (conexion == NetworkAccess.Internet)
+            {
+                if (locl != null)
+                {
+                    locl.PositionChanged += Locl_PositionChanged;
+                    if (!locl.IsListening)
+                    {
+                        await locl.StartListeningAsync(TimeSpan.FromSeconds(10), 100);
+                    }
+
+                    var posicion = await locl.GetLastKnownLocationAsync();
+                    var mapcenter = new Xamarin.Forms.Maps.Position(posicion.Latitude, posicion.Longitude);
+                    mapa.MoveToRegion(new Xamarin.Forms.Maps.MapSpan(mapcenter, 1, 1));
+                }
+            }
+        }
+
+        private void Locl_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            var mapcenter = new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude);
+            mapa.MoveToRegion(new Xamarin.Forms.Maps.MapSpan(mapcenter, 1,1));
+        }
+    }
 }
